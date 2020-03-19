dd46e617ec067681951e367292a78910aaae9a30
Pablo González <pablo.azumo.co>
2020-03-19 15:12:38 -0400

    Add pagination to endpoint

--- app/controllers/reddit_controller.rb
+++ app/controllers/reddit_controller.rb
@@ -1,8 +1,26 @@
 require 'httparty'
+require 'will_paginate/array'
 REDDIT_URL = "https://www.reddit.com/r/"
 class RedditController < ApplicationController
+  before_action :pagination_params
+
   def top
     response = HTTParty.get(REDDIT_URL + "all/.json?count=50")
-    render :json => response.body
+    begin
+      limit = pagination_params[:limit] ? pagination_params[:limit] : "50"
+      page = pagination_params[:page] ? pagination_params[:page] : "1"
+      json = JSON.parse(response.body)
+      data = json["data"]["children"]
+      puts data.length
+      paged = data.paginate(:page => page, :per_page => limit)
+    rescue
+      paged = response.body
+    end
+    render :json => paged
+  end
+
+  private
+  def pagination_params
+    params.permit(:limit, :page)
   end
 end
