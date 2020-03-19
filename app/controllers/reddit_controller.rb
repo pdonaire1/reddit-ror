eb3da04a88b6d3595f416f38df5d295c882be1e8
Pablo González <pablo.azumo.co>
2020-03-19 16:07:19 -0400

    Remove prints

--- app/controllers/reddit_controller.rb
+++ app/controllers/reddit_controller.rb
@@ -5,17 +5,16 @@ class RedditController < ApplicationController
   before_action :pagination_params
 
   def top
-    response = HTTParty.get(REDDIT_URL + "all/.json?count=50", {
+    response = HTTParty.get(REDDIT_URL + "all/.json?count=25", {
       "headers": {
         "User-Agent" => 'reddit-app:v0.0.(by/u/pablo_donaire29)'
       }
     })
     begin
-      limit = pagination_params[:limit] ? pagination_params[:limit] : "50"
-      page = pagination_params[:page] ? pagination_params[:page] : "1"
+      limit = pagination_params[:limit] ? pagination_params[:limit] : 50
+      page = pagination_params[:page] ? pagination_params[:page] : 1
       json = JSON.parse(response.body)
       data = json["data"]["children"]
-      puts data.length
       paged = data.paginate(:page => page, :per_page => limit)
     rescue
       paged = response.body
