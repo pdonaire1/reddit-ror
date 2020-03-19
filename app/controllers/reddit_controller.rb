05b75e9b3b2fa41bb9523b18de3f35c02a966e47
Pablo González <pablo.azumo.co>
2020-03-19 15:06:46 -0400

    Add top endpoint for reddit list

--- app/controllers/reddit_controller.rb
+++ app/controllers/reddit_controller.rb
@@ -1,5 +1,8 @@
 require 'httparty'
 REDDIT_URL = "https://www.reddit.com/r/"
 class RedditController < ApplicationController
-  
+  def top
+    response = HTTParty.get(REDDIT_URL + "all/.json?count=50")
+    render :json => response.body
+  end
 end
